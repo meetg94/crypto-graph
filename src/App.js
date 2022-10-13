902a518be0a4522653763109f606fb9265ff55f2
Meet Guleria <meetguleria94@gmail.com>
2022-10-12 17:44:24 -0700

    intial basics

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import axios from 'axios';
+import { useState, useEffect } from 'react'
 
 function App() {
+
+  const [data, setData] = useState([])
+
+  useEffect(() => {
+    axios.get("https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=50")
+          .then(response => {
+            setData(response.data)
+            console.log(response.data)
+          })
+  }, [])
+
+  const cryptoList = data.map((coin) => {
+     return (
+              <li>
+                {coin.id}
+                </li>
+     )
+   })
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <p>Crypto Chart</p>
+      {cryptoList}
     </div>
   );
 }
