a0c1dd8baf02df69c41a07052e159edf7c5ccb6f
Meet Guleria <meetguleria94@gmail.com>
2022-10-22 20:15:54 -0700

    testing chartjs

--- src/Components/ShowTable.js
+++ src/Components/ShowTable.js
@@ -1,3 +1,4 @@
+import { useState, useEffect } from 'react';
 import Table from '@mui/material/Table';
 import TableBody from '@mui/material/TableBody';
 import TableCell from '@mui/material/TableCell';
@@ -5,13 +6,20 @@ import TableContainer from '@mui/material/TableContainer';
 import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
 import Paper from '@mui/material/Paper';
+import axios from 'axios';
+import { Line } from "react-chartjs-2";
 
 function ShowTable({ data, setId }) {
 
   const handleClick = (coin_id) => {
 
-    const baseUrl = `https://api.coingecko.com/api/v3/coins/{coin_id}/market_chart?vs_currency=usd&days=1`
+    const baseUrl = `https://api.coingecnko.com/api/v3/coins/${coin_id}/market_chart?vs_currency=usd&days=1`
     console.log(coin_id)
+
+    axios.get(baseUrl)
+      .then(response => {
+        console.log(response.data)
+      })
   }
 
   return (
@@ -36,7 +44,7 @@ function ShowTable({ data, setId }) {
                     key={i}
                     sx={{ '&:last-child td, &:last-child th': { border: 0 } }}>
                     <TableCell component="th" scope="row">{coin.market_cap_rank}.</TableCell>
-                    <TableCell component="th" scope="row"><button onClick={() => console.log(coin.id)}>{coin.name}</button></TableCell>
+                    <TableCell component="th" scope="row"><button onClick={() => {handleClick(coin.id)}}>{coin.name}</button></TableCell>
                     <TableCell component="th" scope="row">{coin.symbol.toUpperCase()}</TableCell>
                     <TableCell align="right" scope="row">${coin.current_price.toLocaleString("en-US")}</TableCell>
                     <TableCell align="right" scope="row" style={{color: coin.price_change_percentage_24h < 0 ? "red" : "green"}}>
